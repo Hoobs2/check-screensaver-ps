9ce25dd2e37957c03faea2cb43194b45a0f86f88
Davey <41457078+Hoobs2@users.noreply.github.com>
2024-01-11 14:41:31 -0800

    Created detection and remediation script
    Meant to deploy as Intune remediation script.
    Detection Script detects issue and exits with 1 if detected, triggering the remediation script via Intune.
    Remediation script (Set-ScreenSaverTimeOut) sets screen saver wait time in registry for all users.

--- Detection-ScreenSaverTimeOut.ps1
+++ Detection-ScreenSaverTimeOut.ps1
@@ -1,60 +1,99 @@
-<#
-Davey Hobbs
-2024/01/10
-Remediation Detection Script
-#>
-
-#Test ITS Log location and create folder location if it doesnt exist.
+# Test ITS Log location and create folder location if it doesn't exist.
 function TestITSLog {
-    param ()
+    param()
 
     if (!(Test-Path "C:\itslog\Intune")) {
         New-Item -ItemType Directory -Path "C:\itslog\Intune" -Force -ErrorAction SilentlyContinue > $null
     }
-    
+}
+#Get all User SIDs from Local Machine and add them to an ordered HashTable.
+function Get-UserSIDs {
+    $profileListPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
+    $userSIDs = Get-ChildItem -Path $profileListPath | ForEach-Object {
+        $_.PSChildName
+    }
+
+    $userSIDsTable = [ordered]@{}
+    foreach ($sid in $userSIDs) {
+        $userSIDsTable[$sid] = $null
+    }
+
+    return $userSIDsTable
 }
 
+#Loop through User SIDs ordered hashtable, detect if $valueName is less than 
 function Test-RegistryValue {
-    param(
-        [string]$regPath,
-        [string]$valueName
+    param (
+        [hashtable]$userSIDsTable,
+        [string]$valueName,
+        [string]$newValue,
+        [string]$logFilePath,
+        [string]$regPathHive,
+        [string]$regPath
     )
 
     try {
-        $registryValue = Get-ItemPropertyValue -Path $regPath -Name $valueName -ErrorAction Stop
+        foreach ($userSID in $userSIDsTable.Keys) {
+            $currentRegPath = "$regPathHive$userSID$regPath"
+                        
 
-        if ($null -eq $registryValue) {
-            $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry value '$valueName' not found in path '$regPath'."
-            Add-content -Path $logFilePath -Value $logMessage
-            EXIT 0
-        }
+            if ( (-not (Test-Path Registry::$currentRegPath)) -or (-not ((Get-Item Registry::$currentRegPath).Property -contains $valuename))) {
+                
+                continue
+            }
+            else {
+                $registryValue = Get-ItemPropertyValue -Path Registry::$currentRegPath -Name $valueName -ErrorAction SilentlyContinue
 
-        if ($registryValue -eq 600) {
-            $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry key '$valueName' under '$regPath' was equle to 600."
-            Add-content -Path $logFilePath -Value $logMessage
-            EXIT 0
-        } elseif ($registryValue -lt 600) {
-            $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry key '$valueName' under '$regPath' was less than to 600."
-            Add-content -Path $logFilePath -Value $logMessage
-            EXIT 1
-        } else {
-            $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry key '$valueName' under '$regPath' was greater then 600."
-            Add-content -Path $logFilePath -Value $logMessage
-            EXIT 0
+                switch ($registryValue) {
+                    {$_ -eq $newValue} {
+                        $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry key '$valueName' under '$currentRegPath' was equal to 600."
+                        Add-content -Path $logFilePath -Value $logMessage
+                        return 0
+                    }
+                    {$_ -gt $newValue} {
+                        $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry key '$valueName' under '$currentRegPath' was greater than 600."
+                        Add-content -Path $logFilePath -Value $logMessage
+                        return 0
+                    }
+                    {$_ -lt $newValue} {
+                        $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry key '$valueName' under '$currentRegPath' was less than 600."
+                        Add-content -Path $logFilePath -Value $logMessage
+                        EXIT 1
+                    }
+                    {$_ -eq $null} {
+                        $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry value '$valueName' not found in path '$currentRegPath'."
+                        Add-content -Path $logFilePath -Value $logMessage
+                    }
+                }
+            }
         }
+    #Could move the catch up so that it does not include the foreach, allowing to continue the loop if an error occures. Would get rid of if statment?
     } catch {
         $errorMessage = "Error: $($_.Exception.Message)"
-        $logErrorMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Failed to change registry key '$valueName' under '$regPath'. $errorMessage"
+        $logErrorMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Failed due to error. '$valueName' under '$currentRegPath'.  $errorMessage"
         Add-content -Path $logFilePath -Value $logErrorMessage
-
     }
 }
 
-# Example usage of the function
-$regPath = "HKCU:\Control Panel\Desktop"
-$valueName = "ScreenSaveTimeOut"
+#Define variables
 $logFilePath = "C:\itslog\Intune\DETECTIONScreenSaverTimeOut.txt"
+$regPathHive = "HKEY_USERS\"
+$regPath = "\Control Panel\Desktop"
+$valueName = "ScreenSaveTimeOut"
+$newValue = "600"
 
+
+
+#Launch Functions
 TestITSLog
-Test-RegistryValue -regPath $regPath -valueName $valueName
 
+# Get User SIDs and create hashtable
+$userSIDsTable = Get-UserSIDs
+
+# Check ScreenSaverTimeout for each user in the hashtable
+if ((Test-RegistryValue -userSIDsTable $userSIDsTable -valueName $valueName -newValue $newValue -logFilePath $logFilePath -regPathHive $regPathHive -regPath $regPath) -eq 0) {
+    Exit 0
+}
+else {
+    Exit 1
+}
--- Set-ScreenSaverTimeOut.ps1
+++ Set-ScreenSaverTimeOut.ps1
@@ -1,7 +1,7 @@
 <#
 Davey Hobbs
 2024/01/10
-
+Create ITSLog location, Get User SIDs from local machine and add to Ordered hashtable, loop through hashtable and change registry value if it exists.
 #>
 
 #Test ITS Log location and create folder location if it doesnt exist.
@@ -14,31 +14,92 @@ function TestITSLog {
     
 }
 
+#Get all User SIDs from Local Machine and add them to an ordered HashTable.
+function Get-UserSIDs {
+    $profileListPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
+    $userSIDs = Get-ChildItem -Path $profileListPath | ForEach-Object {
+        $_.PSChildName
+    }
+
+    $userSIDsTable = [ordered]@{}
+    foreach ($sid in $userSIDs) {
+        $userSIDsTable[$sid] = $null
+    }
+
+    return $userSIDsTable
+}
+
 #Set Registry Key and write log.
 function Set-RegistryKeyValue {
     param(
+        [hashtable]$userSIDsTable,
+        [string]$regPathHive,
         [string]$regPath,
         [string]$valueName,
         [string]$newValue,
-        [string]$logFilePath
+        [string]$logFilePath 
     )
-
+    #Try block and Catch error and write to log file. Could try moving to inside loop to catch errors and continue loop, possibly allowing the removal of the internal if statment.
     try {
-        Set-ItemProperty -Path $regPath -Name $valueName -Value $newValue -ErrorAction Stop
-        $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry key '$valueName' under '$regPath' changed to '$newValue' successfully."
-        Add-content -Path $logFilePath -Value $logMessage
+        #Loop through entire userSID ordered hashtable, and evaluate ScreenSaveTimeOut value.
+        foreach ($userSID in $userSIDsTable.Keys) {
+            $currentRegPath = "$regPathHive$userSID$regPath"
+
+            #If the path does not exist or the item ScreenSaveTimeOut under HKEY_USERS\{SID}\Control Panel\Desktop does not exist but the path does, continue to next loop iteration. Else move to further evaluation. (Could move into Switch at somepoint.)
+            if ( (-not (Test-Path Registry::$currentRegPath)) -or (-not ((Get-Item Registry::$currentRegPath).Property -contains $valuename))) {
+                continue
+            }
+            else {
+                $registryValue = Get-ItemPropertyValue -Path Registry::$currentRegPath -Name $valueName -ErrorAction SilentlyContinue
+
+                #Evaluate ScreenSaveTimeOut value.
+                switch ($registryValue) {
+                    #If ScreenSaveTimeOut is equal to 600, continue to next loop iteration.
+                    {$_ -eq $newValue} {
+                        $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry key '$valueName' under '$currentRegPath' was equal to 600."
+                        Add-content -Path $logFilePath -Value $logMessage
+                        continue
+                    }
+                    #If ScreenSaveTimeOut is greater than 600, continue to next loop iteration.
+                    {$_ -gt $newValue} {
+                        $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry key '$valueName' under '$currentRegPath' was greater than 600."
+                        Add-content -Path $logFilePath -Value $logMessage
+                        continue
+                    }
+                    #If ScreenSaveTimeOut is set to less then 600, set value to 600.
+                    {$_ -lt $newValue} {
+                        Set-ItemProperty -Path Registry::$currentRegPath -Name $valueName -Value $newValue -ErrorAction SilentlyContinue
+                        $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry key '$valueName' under '$currentRegPath' was less than 600."
+                        Add-content -Path $logFilePath -Value $logMessage
+                        continue
+                    }
+                    #If ScreenSaveTimeOut exists but does not have a value, set value to 600
+                    {$_ -eq $null} {
+                        Set-ItemProperty -Path Registry::$currentRegPath -Name $valueName -Value $newValue -ErrorAction SilentlyContinue
+                        $logMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Registry value '$valueName' under '$currentRegPath' did not have a value."
+                        Add-content -Path $logFilePath -Value $logMessage
+                        continue
+                    }
+                }
+            }
+        }
     } catch {
         $errorMessage = "Error: $($_.Exception.Message)"
-        $logErrorMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Failed to change registry key '$valueName' under '$regPath'. $errorMessage"
+        $logErrorMessage = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - Failed due to error. '$valueName' under '$currentRegPath'.  $errorMessage"
         Add-content -Path $logFilePath -Value $logErrorMessage
     }
 }
 
 #Set Variable Values
-$regPath = "HKCU:\Control Panel\Desktop"
+$regPath = "\Control Panel\Desktop"
+$regPathHive = "HKEY_USERS\"
 $valueName = "ScreenSaveTimeOut"
 $newValue = "600"
 $logFilePath = "C:\itslog\Intune\ScreenSaverTimeOut.txt"
 
+#Get User SIDs and create hashtable
+$userSIDsTable = Get-UserSIDs
+
+#Call functions.
 TestITSLog
-Set-RegistryKeyValue -regPath $regPath -valueName $valueName -newValue $newValue -logFilePath $logFilePath
+Set-RegistryKeyValue -userSIDsTable $userSIDsTable -regPathHive $regPathHive -regPath $regPath -valueName $valueName -newValue $newValue -logFilePath $logFilePath
